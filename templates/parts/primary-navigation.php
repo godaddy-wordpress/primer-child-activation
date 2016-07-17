ed5cd5722cf4e9b08de5efaae4f8c83363dbd627
Matt Pritchett <mpritchett@crowdfavorite.com>
2016-07-17 15:33:18 -0400

    Add search bar to primary navigation action.

---
+++ templates/parts/primary-navigation.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Displays the primary navigation.
+ *
+ * @package Primer
+ */
+?>
+
+<div class="main-navigation-container">
+
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+
+		<div class="menu-toggle" id="menu-toggle">
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ) ?>
+
+		<?php get_search_form(); ?>
+
+	</nav><!-- #site-navigation -->
+
+</div>

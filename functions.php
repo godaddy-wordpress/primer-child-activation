52e1162479cf62291d8856dee422a2522b165d83
Matt Pritchett <mpritchett@crowdfavorite.com>
2016-08-03 20:27:41 -0400

    Removing duplicate styles

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@
  */
 function activation_theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 
 	wp_enqueue_script( 'primer-navigation' );
 }
